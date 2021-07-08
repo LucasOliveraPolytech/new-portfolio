c7ca8e4b0015a2b7f8ae1bde4c5864a60e49c810
Lucas Olivera <oliveralucasm@gmail.com>
2021-07-08 20:29:22 -0300

    + adds to Bio and Tech objs

--- src/Bio.svelte
+++ src/Bio.svelte
@@ -5,7 +5,7 @@
 <div class='bio-container'>
   <div class='bio' in:fade={{delay: 300, duration: 500}} out:fade>
     <p>El proyecto AgileTalk comenzó en 2020 como una competencia entre dos equipos de aproximadamente 30 desarrolladores cada uno. Todos alumnos de la carrera de Ingeniaria en Sistemas de la Universidad Nacional del Centro de la Provincia de Buenos Aires, siendo parte de la materia Diseño de Software. La competencia concistió en presentar un ambiente virtual 3D didactico que permitiera a otros alumnos interiorizarse en distintos procesos de desarrollo ágil (idealmente pudiendo definirlos dentro del ambiente). Esto manteniendo una organización y siguiendo un proceso. La competencia nos dejó con dos acercamientos similares, dado que los requerimientos se fueron dando a lo largo de la cursada a ambos equipos por igual, durante las clases.</p>
-    <p>A comienzos del 2021, dentro del marco de la materia Ingenieria de Sofware, la premisa fue encarar un nuevo desarrollo, pudiendo tomar como base los desarrollados en el 2020, siguiendo los elementos escenciales de SAFe (<a href='https://www.scaledagileframework.com/' target="_blank" rel="noopener noreferrer">Scaled Agile Framework</a>) unificando ambos grupos en una pequeña empresa de 37 empleados. </p>
+    <p>A comienzos del 2021, dentro del marco de la materia Ingenieria de Sofware, la premisa fue encarar un nuevo desarrollo, pudiendo tomar como base los desarrollados en el 2020, siguiendo los elementos escenciales de SAFe (<a href='https://www.scaledagileframework.com/' target="_blank" rel="noopener noreferrer" style="color: orangered;">Scaled Agile Framework</a>) unificando ambos grupos en una pequeña empresa de 37 empleados. </p>
   </div>
 </div>
 
--- src/Tech.svelte
+++ src/Tech.svelte
@@ -3,14 +3,20 @@
 
   const features = [
     {
-      title: "Lenguaje Natural",
-      description: "<p></p>",
+      title: "AgileBots",
+      description: "<p>El centro de AgileTalk son los llamados \"AgileBots\". En principio son agentes con la capacidad de ser personificados por un Team Member o actuar como un representante del mismo ante determinadas situaciones (reuniones, por ejemplo). Tambien hay otros que son parte del sistema como el Process Action Bot que si bien tiene un avatar dentro del mundo no puede ser personificado.</p>",
       outLink: "https://catalyticsound.com/quarterly/8/index.html",
       media: "images/cq-8.png",
     },
     {
-      title: "Control de proceso",
-      description: "<p></p>",
+      title: "Lenguaje Natural",
+      description: "<p>AgileTalk está diseñado para que la gran parte de las interacciones se hagan dentro del ambiente virtual utilizando el lenguaje natural.</p>",
+      outLink: "https://catalyticsound.com/quarterly/8/index.html",
+      media: "images/cq-8.png",
+    },
+    {
+      title: "Definición y Control de Proceso",
+      description: "<p>AgileTalk puede ser configurado para seguir un proceso de desarrollo definido previamente. Dicho seguimiento está a cargo del Process Action Bot, quien tiene la característica de reportar ante el Scrum Master distintas métricas a diariamente.</p>",
       outLink: "https://catalyticsound.com/quarterly/9/index.html",
       media: "images/cq-9.png"
     },
@@ -21,17 +27,17 @@
     },
     {
       title: "Simulación",
-      description: "<p>Dentro de AgileTalk es posible recrear el flujo de eventos luego de haberse dado dentro del ambiente.</p>",
+      description: "<p>Dentro de AgileTalk es posible recrear el flujo de eventos luego de haberse dado dentro del ambiente, estando inmerso en el mundo al momento de la reproducción.</p>",
       media: "images/catalyticsoundstream.png"
     },
     {
-      title: "Guía del Proceso",
+      title: "Asistente de Scrum",
       description: "<p>Como herramienta didáctica, AgileTalk permite entrenar a alguien en un nuevo proceso de desarrollo guiándolo a traves de las distintas ceremonias y artefactos.</p>",
       media: "images/catalyticsoundstream.png"
     },
     {
       title: "Personalizable",
-      description: "<p>Aún en desarrollo. Manteniendo un monitoreo de las distintas acciones de un Team Memver a lo largo de un proyecto sería posible inferir en su personalidad y por ende personalizar sus respuestas y acciones en base a la misma cuando esté desconectado</p><p>Aún en desarrollo. Manteniendo un monitoreo de las distintas acciones de un Team Memver a lo largo de un proyecto sería posible inferir en su personalidad y por ende personalizar sus respuestas y acciones en base a la misma cuando esté desconectado</p><p>Aún en desarrollo. Manteniendo un monitoreo de las distintas acciones de un Team Memver a lo largo de un proyecto sería posible inferir en su personalidad y por ende personalizar sus respuestas y acciones en base a la misma cuando esté desconectado</p>",
+      description: "<p>Aún en desarrollo. Manteniendo un monitoreo de las distintas acciones de un Team Member a lo largo de un proyecto sería posible inferir en su personalidad y por ende personalizar sus respuestas y acciones en base a la misma cuando esté desconectado</p>",
       media: "images/catalyticsoundstream.png"
     },
   ]
